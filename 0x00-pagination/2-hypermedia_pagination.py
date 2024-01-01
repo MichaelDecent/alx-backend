2fb930832d6595de898cae0c0864a113002eed0f
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-01-02 00:57:59 +0100

    added solution to task 3

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -57,7 +57,7 @@ class Server:
         except IndexError:
             return []
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
         """ """
         assert type(page) is int and page > 0
         assert type(page_size) is int and page_size > 0
