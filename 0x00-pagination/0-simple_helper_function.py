83d4efe8bceb7354d4661bed0dbef5b05f359caf
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-01-01 21:18:22 +0100

    added task o solution

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,8 +2,10 @@
 """
 a function named index_range that takes two integer arguments page and page_size
 """
+
+
 def index_range(page: int, page_size: int) -> tuple[int, int]:
-    if page <=0 and page_size <= 0:
+    if page <= 0 and page_size <= 0:
         return None
     start_index = (page - 1) * page_size
     end_index = page * page_size
