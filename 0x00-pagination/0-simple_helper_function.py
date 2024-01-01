16279276e1ce45520bfaf5902d1a7a08d57fc639
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-01-01 21:21:26 +0100

    added task to solution

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,9 +3,10 @@
 Tis Module contains a function named index_range that takes
 two integer arguments page and page_size
 """
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     return a tuple of size two containing a start index
     and an end index corresponding to the range of indexes
