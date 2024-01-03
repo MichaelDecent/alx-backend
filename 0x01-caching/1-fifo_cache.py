46fd51f6b62f9b06b2e9970a66bc4e6331d57e2f
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-01-03 23:57:47 +0100

    added solution to task 2

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This Module contain a class FIFO Cache that inherites from BaseCaching
+"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    FIFO Cache Algorithm
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key and item:
+            self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            discarded_key = (
+                list(self.cache_data.keys())[0] if len(
+                    self.cache_data) > 0 else None)
+            del self.cache_data[discarded_key]
+            print(f"DISCARD: {discarded_key[0]}")
+
+    def get(self, key):
+        """Get an item by key"""
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data.get(key)
---
+++ 0x01-caching/test.py
@@ -0,0 +1,7 @@
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+
+# Remove and return the last inserted key-value pair
+removed_item = my_dict['a']
+print("Removed item:", removed_item)
+
+print("Updated dictionary:", my_dict)
