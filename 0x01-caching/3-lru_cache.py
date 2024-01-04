692cf3563b769df9645c90bfc2416d245c744323
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-01-04 03:31:28 +0100

    added solution to task 4 & 5

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python3
-"""This module contains the implementation of the LIFO -
-Last In First Out caching policy"""
+"""This module contains the implementation of
+one of the caching policy, LFU - Least Frequently
+Used"""
 
 from base_caching import BaseCaching
 from collections import OrderedDict
 
 
-class LIFOCache(BaseCaching):
-    """clas that implementd LIFO cache policy"""
-
+class LRUCache(BaseCaching):
+    """Class oimplementation of LRU cache policy"""
     def __init__(self):
-        """Initialize object instances at creation"""
         super().__init__()
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """Add new item to the cache storage"""
+        """Add new item to cache dict"""
         if key is None or item is None:
             return
-        # If new key is being added
+        # If new item is being added to the cache
         if key not in self.cache_data:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                discarded_item = self.cache_data.popitem(last=True)
-                print("DISCARD: {}".format(discarded_item[0]))
-
-        # If a key's value is being updated, update and move it to the last
+                discarded_item = self.cache_data.popitem(last=False)
+                print('DISCARD: {}'.format(discarded_item[0]))
         self.cache_data[key] = item
         self.cache_data.move_to_end(key, last=True)
 
     def get(self, key):
-        """Get data from cache storage"""
+        """Access cache staorage and move recently accessed data
+        to the end"""
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=True)
         return self.cache_data.get(key, None)
