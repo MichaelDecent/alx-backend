f2861d1a2d27911499da199d1d3e40f7a6bd6d18
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-01-06 21:30:58 +0100

    added advanced

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,36 +1,39 @@
-#!usr/bin/env python3
-"""This module contains one of the common caching policy
-LFU - Least Frequently Used"""
-
+#!/usr/bin/env python3
+"""This module contains implementation of one of the
+caching policie, MRU - Most Recently Used"""
+from collections import OrderedDict
 from base_caching import BaseCaching
 
 
 class LFUCache(BaseCaching):
-    """class implementation of LFU cache policy"""
-    def __init__(self) -> None:
-        """Initialize of class instance on creation """
+    """Class implementation of  LFU caching policy"""
+
+    def __init__(self):
         super().__init__()
-        self.track_dict = {}
+        self.cache_data = OrderedDict()
+        self.key_frequency_dict = {}
 
     def put(self, key, item):
-        """Add new item in the cache"""
+        """Add item/data to the cache storage"""
         if key is None or item is None:
             return
-        self.cache_data[key] = item
-        if len(self.cache_data) > self.MAX_ITEMS:
-            pop_item = min(self.track_dict, key=self.track_dict.get)
-            self.track_dict.pop(pop_item)
-            self.cache_data.pop(pop_item)
-            print("DISCARD: {}".format(pop_item))
 
-        if key not in self.track_dict:
-            self.track_dict[key] = 0
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            least_frequent_key = min(
+                self.key_frequency_dict, key=self.key_frequency_dict.get
+            )
+            self.key_frequency_dict.pop(least_frequent_key)
+            self.cache_data.pop(least_frequent_key)
+            print("DISCARD: {}".format(least_frequent_key))
+
+        if key not in self.key_frequency_dict:
+            self.key_frequency_dict[key] = 0
         else:
-            self.track_dict[key] += 1
+            self.key_frequency_dict[key] += 1
 
     def get(self, key):
-        """Acces the cache storage for item"""
-        if key is None or key not in self.cache_data:
-            return None
-        self.track_dict[key] += 1
-        return self.cache_data.get(key)
+        """Retrieve item from the cache"""
+        if key is not None and key in self.cache_data:
+            self.key_frequency_dict[key] += 1
+        return self.cache_data.get(key, None)
