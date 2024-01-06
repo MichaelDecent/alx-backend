445d67a5331b10fa9541a965e913698234f2e972
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-01-06 21:31:17 +0100

    added advanced

--- 0x01-caching/test.py
+++
@@ -1,7 +0,0 @@
-# Sample dictionary
-my_dict = {'a': 10, 'b': 20, 'c': 15, 'd': 30}
-
-# Getting the maximum key from the dictionary
-max_key = max(my_dict.keys())
-
-print("Maximum key in the dictionary:", max_key)
